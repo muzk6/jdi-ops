576c9f37a0e882e1f23cfc0bd84af2b24ad5f1cb
muzk6 <muzk6x@gmail.com>
2020-10-12 09:47:09 +0800

    config.debug => false

--- init.php
+++ init.php
@@ -17,6 +17,7 @@ require PATH_ROOT . '/vendor/autoload.php';
 
 // 框架初始化
 \JDI\App::init([
+    'config.debug' => false,
     'config.path_data' => PATH_ROOT . '/data',
     'config.path_view' => PATH_ROOT . '/views',
     'config.path_config_first' => PATH_ROOT . '/config',
